c69933e8b8fa29ff0f38abd7035fe75c9c7a52f1
Jakub Czapliński <itsupgradetime@gmail.com>
2017-06-07 19:04:12 +0200

    Prosty model.

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name='hello'
+
 urlpatterns = [
+    url(r'^test/$', views.index, name='index'),
     url(r'^json/$', views.hello_json, name="hello_json"),
     url(r'^$', views.hello_show, name="hello_show"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from hello.models import ShoppingItem
+
+
+def index(request):
+    items = ShoppingItem.objects.all()
+    return render(request, 'hello/test.html', context={'items': items})
 
 def hello_show(request):
     return render(request, 'hello/show.html', {})
