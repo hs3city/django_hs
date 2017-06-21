7f52f9572e5408eb7a5f8ec23d28163e7a61e6f0
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2017-06-21 19:45:39 +0200

    Added method __str__ to models.

--- hello/models.py
+++ hello/models.py
@@ -9,15 +9,31 @@ class User(models.Model):
     job = models.ForeignKey('Job')
     section = models.ForeignKey('Section')
 
+    class Meta:
+        verbose_name = "użytkownik"
+        verbose_name_plural = "użytkownicy"
+
+    def __str__(self):
+        return self.name
+
 
 class Job(models.Model):
     jobtitle = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.jobtitle
 
 
 class Department(models.Model):
     name = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.name
+
 
 class Section(models.Model):
     name = models.CharField(max_length=120)
     department = models.ForeignKey('Department')
+
+    def __str__(self):
+        return self.name
