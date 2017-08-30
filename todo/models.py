4daa5e183cb907b056d35c2980c28f684ae9af85
Piotr Dobrysiak <piotr.dobrysiak@gmail.com>
2017-08-30 19:18:26 +0200

    Models ToDo & Category created.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Kategoria'
+        verbose_name_plural = 'Kategorie'
+
+
+class ToDo(models.Model):
+    TODO = 'TD'
+    DONE = 'DN'
+    STATUS = (
+        (TODO, 'Aktywny'),
+        (DONE, 'Wykonane')
+    )
+    DIFFICULTY = (
+        (1, 'Od ręki'),
+        (3, 'Prosty'),
+        (5, 'Średnie'),
+        (8, 'Trudne'),
+        (13, 'Bardzo trudne'),
+    )
+    name = models.CharField(max_length=250)
+    description = models.TextField(verbose_name='Opis')
+    date_created = models.DateField(auto_now_add=True)
+    date_deadline = models.DateField(default=None)
+    status = models.CharField(max_length=2, choices=STATUS)
+    active = models.BooleanField(default=True)
+    time_expected = models.IntegerField(default=15)
+    difficulty = models.IntegerField(choices=DIFFICULTY)
+    category = models.ForeignKey(Category)
+
+
